f9f8d1816b64ab85a31d88a4b4410b9b47115429
adityamane3011-9999 <adityamane.3011@gmail.com>
2025-09-04 08:11:59 +0530

    Create HUD.ts

---
+++ src/src/core/src/input/src/ui/HUD.ts
@@ -0,0 +1,17 @@
+export class HUD {
+  private element: HTMLElement;
+
+  constructor() {
+    this.element = document.getElementById('hud') as HTMLElement;
+    if (!this.element) {
+      console.error("HUD element not found! Make sure you have a <div id='hud'> in your HTML.");
+    }
+  }
+
+  public update(text: string): void {
+    if (this.element) {
+      // Use <pre> to preserve whitespace and newlines
+      this.element.innerHTML = `<pre>${text}</pre>`;
+    }
+  }
+}
