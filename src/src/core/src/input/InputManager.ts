7ff94ea0cdd047c6af43a4c9738becf0f22f0071
adityamane3011-9999 <adityamane.3011@gmail.com>
2025-09-04 08:11:17 +0530

    Create InputManager.ts

---
+++ src/src/core/src/input/InputManager.ts
@@ -0,0 +1,20 @@
+export class InputManager {
+  private readonly pressedKeys: Set<string> = new Set();
+
+  constructor() {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    window.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
+
+  private handleKeyDown(event: KeyboardEvent): void {
+    this.pressedKeys.add(event.key.toLowerCase());
+  }
+
+  private handleKeyUp(event: KeyboardEvent): void {
+    this.pressedKeys.delete(event.key.toLowerCase());
+  }
+
+  public isKeyPressed(key: string): boolean {
+    return this.pressedKeys.has(key.toLowerCase());
+  }
+}
